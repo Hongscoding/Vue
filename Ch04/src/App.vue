9d45015f290d2235380dd348a7e7c19f8a9a78b8
alswns3906 <alswns3906@naver.com>
2023-04-07 10:51:58 +0900

    2023-04-07 router

--- Ch04/src/App.vue
+++ Ch04/src/App.vue
@@ -10,9 +10,12 @@
   <hr />
 
   <h4>3-LifeCycle Hooks</h4>
+  <HooksComponent v-if="isShow"></HooksComponent>
+  <button @click="btnToggle">컴포넌트 {{ status }}</button>
   <hr />
 
   <h4>4-Dependency injection</h4>
+  <DIComponent></DIComponent>
   <hr />
 </template>
 
@@ -26,12 +29,46 @@
  *  - 기존 Options API를 대체하는 통합한 Composition API
  *  - 컴포넌트의 로직을 유연하고 가독성 있게 처리할 수 있는 함수 기반 API
  */
+import { provide, reactive, ref } from 'vue';
 import Component1 from './components/sub1/Component1.vue';
 import MyComponent1 from './components/sub2/MyComponent1.vue';
+import HooksComponent from './components/sub3/HooksComponent.vue';
+import DIComponent from './components/sub4/DIComponent.vue';
+import DISubComponent from './components/sub4/DISubComponent.vue';
 
 export default {
     name: "App",
-    components: { Component1, MyComponent1 }
-}
+    components: { Component1, MyComponent1, HooksComponent, DIComponent, DISubComponent },
+    setup(){
+      const isShow = ref(true);
+      const status = ref("삭제");
+
+      const user = reactive({
+        name: "김유신",
+        age:23,
+        addr:"김해시",
+      });
+
+      // App 컴포넌트 하위 모든 컴포넌트에서 inject를 통해 user 참조 가능
+      provide("user", user);
+
+      function btnToggle(){
+        if(isShow.value){
+          isShow.value = false;
+          status.value = "생성";
+        }else{
+          isShow.value = true;
+          status.value = "삭제";
+        }
+      };
+
+      return{
+        isShow,
+        status,
+        btnToggle,
+
+      };
+    },
+};
 </script>
 
---
+++ Ch05/src/components/sub1/Welcome.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h4>welcome</h4>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+div{
+    border: 1px solid green;
+}
+</style>
